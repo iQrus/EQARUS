c12d68101b933c8619062c3167b038890fd76ffe
nandkrish <nandkrish43@gmail.com>
2019-10-14 22:18:01 +0530

    new changes

---
+++ Eqarus-Google-Semantic/src/main/java/com/google/semantic/constants/GoogleSemanticConstants.java
@@ -0,0 +1,10 @@
+package com.google.semantic.constants;
+
+public interface GoogleSemanticConstants {
+	
+	String SENTIMENT_WRAPPER_RESPONSE_LOGGER="Inside the method SentimentWrapperResponse getLanguageAnalysis()";
+
+	String GOOGLE_DOCUMENT_PROCESS_SUCCESSFULLY="Document has been processed succesfully & now it is ready for AnalyzeSentimentResponse";
+	
+	String GOOGLE_ANALYZESENTIMENT_RESPONSE_SUCCESSFULLY="AnalyzeSentimentResponse processed SuccessFully";
+}
